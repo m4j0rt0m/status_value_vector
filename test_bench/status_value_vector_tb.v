d2bd043489dd069435c27d1007b839df444507a1
m4j0rt0m <major.tom.spaceworm@gmail.com>
2019-08-06 02:06:01 +0200

    updating test bench

--- test_bench/status_value_vector_tb.v
+++ test_bench/status_value_vector_tb.v
@@ -7,7 +7,6 @@
  */
 module status_value_vector_tb ();
 
-
   /* local parameters */
   localparam  FREQ_CLK  = 50000000;
   localparam  CLK_F     = (1000000000 / FREQ_CLK) / 2;
@@ -18,14 +17,16 @@ module status_value_vector_tb ();
   `define CYCLES(cycles)  (CLK_F*2*cycles)
 
   /* regs and wires */
-  reg               clk_i;    //..clock signal
-  reg               rsn_i;    //..active low reset
-  reg               push_i;   //..push a new status entry
-  reg               pull_i;   //..pull the next oldest entry
-  reg   [WIDTH-1:0] value_i;  //..value to be inserted into queue
-  wire  [WIDTH-1:0] value_o;  //..next value from status vector
-  wire              valid_o;  //..valid entry in the status vector
-  wire              full_o;   //..status vector is full
+  reg               clk_i;        //..clock signal
+  reg               rsn_i;        //..active low reset
+  reg               push_i;       //..push a new status entry
+  reg               pull_i;       //..pull the next oldest entry
+  reg   [WIDTH-1:0] value_i;      //..value to be inserted into queue
+  wire  [WIDTH-1:0] value_o;      //..next value from status vector
+  wire              valid_o;      //..valid entry in the status vector
+  wire              full_o;       //..status vector is full
+  reg               set_i;        //..reupdate last value
+  reg   [WIDTH-1:0] set_value_i;  //..reupdate value to be set
 
   /* dut */
   status_value_vector
@@ -41,7 +42,9 @@ module status_value_vector_tb ();
         .value_i (value_i),
         .value_o (value_o),
         .valid_o (valid_o),
-        .full_o (full_o)
+        .full_o (full_o),
+        .set_i (set_i),
+        .set_value_i (set_value_i)
       );
 
   /* initial */
@@ -58,18 +61,61 @@ module status_value_vector_tb ();
     #CLK_F  clk_i <=  ~clk_i;
   end
 
+  /* clog2 function */
+  function integer clog2;
+    input integer value;
+    begin
+      value = value-1;
+      for (clog2=0; value>0; clog2=clog2+1)
+        value = value>>1;
+    end
+  endfunction
+
   /* simulation */
   always  begin
     #`CYCLES(4)   rsn_i = 1;
-    #`CYCLES(1)   push_i  = 1;
-    #`CYCLES(4)   push_i  = 0;
-                  pull_i  = 1;
-    #`CYCLES(5)   push_i  = 1;
-    #`CYCLES(2)   push_i  = 0;
-                  pull_i  = 0;
+    #`CYCLES(2)   push_t(10);
+    #`CYCLES(1)   pull_t(5);
+//    #`CYCLES(1)   push_i  = 1;
+//    #`CYCLES(4)   push_i  = 0;
+//                  pull_i  = 1;
+//    #`CYCLES(5)   push_i  = 1;
+//    #`CYCLES(2)   push_i  = 0;
+//                  pull_i  = 0;
 //    #`CYCLES(2)   pull_i  = 1;
-    #`CYCLES(6)   push_i  = 1;
+//    #`CYCLES(6)   push_i  = 1;
   end
+
+  /* push data task */
+  reg [$clog2(DEPTH)-1:0] push_counter;
+  task automatic push_t;
+    input [$clog2(DEPTH)-1:0] push_amount;
+    begin
+      push_counter  = 0;
+      while (push_counter != push_amount) begin
+        push_i  = 1;
+        #`CYCLES(1);
+        push_counter = push_counter + 1;
+        push_i  = 0;
+      end
+    end
+  endtask
+
+  /* pull data task */
+  reg [$clog2(DEPTH)-1:0] pull_counter;
+  task automatic pull_t;
+    input [$clog2(DEPTH)-1:0] pull_amount;
+    begin
+      pull_counter  = 0;
+      while (pull_counter != pull_amount) begin
+        pull_i  = 1;
+        #`CYCLES(1);
+        pull_counter = pull_counter + 1;
+        pull_i  = 0;
+      end
+    end
+  endtask
+
   always  begin
     $dumpfile("status_value_vector.vcd");
     $dumpvars();
